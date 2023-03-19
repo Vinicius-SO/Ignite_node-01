91b724a3d1138df6e43d908d0411c83da30d3e95
Vinicius-SO <vhisoares39@gmail.com>
2023-03-19 11:40:13 -0300

    method verification

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+export const routes = [
+    {
+        method: 'GET',
+        path:'/users',
+        handler: (req, res) =>{
+            const users = database.select('users')
+            return res.end(JSON.stringify(users))
+        }
+    },
+    {
+        method: 'POST',
+        path:'/users',
+        handler: (req, res) =>{
+            const { name , email } = req.body
+            const user = {
+                id: uuid(),
+                name,
+                email
+            }
+    
+            database.insert('users', user )
+            return res.writeHead(201).end()
+        }
+    }
+]
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from 'node:http'
 import { Database } from './database.js'
+import { randomUUID as uuid} from 'node:crypto'
 
 import { json } from './middlewares/json.js'
 
@@ -11,22 +12,12 @@ const server = http.createServer(async (req,res)=>{
 
     await json(req,res)
 
-    if(method === 'GET' && url === '/users'){
-        const users = database.select('users')
-        return res
-        .end(JSON.stringify(users))
-    }
+    const route = route.find(route =>{
+        return route.method === method && route.path === url
+    })    
 
-    if(method === 'POST' && url === '/users'){
-        const { name , email } = req.body
-        const user = {
-            id:1,
-            name,
-            email
-        }
-
-        database.insert('users', user )
-        return res.writeHead(201).end()
+    if(route){
+        return route.handler(req,res)
     }
 
     return res.writeHead(404).end()
