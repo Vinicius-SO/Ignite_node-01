2606990857c21480959f4753690772655196ff98
Vinicius-SO <vhisoares39@gmail.com>
2023-03-20 13:18:24 -0300

    Dealing with Query params

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -2,7 +2,7 @@ export function buildRoutePath(path){
     const routeParameteresRegex = /:([a-zA-Z]+)/g
     const pathWithParams = path.replaceAll(routeParameteresRegex, '(?<$1>[a-z0-9\-_]+)')
     
-    const pathRegex = new RegExp(`^${pathWithParams}`)
+    const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`)
 
     return pathRegex
 } 
